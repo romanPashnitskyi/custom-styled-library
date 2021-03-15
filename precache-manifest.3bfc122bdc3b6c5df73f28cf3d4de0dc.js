77870e709f901801c5ed92700a207505f081fed6
romanPashnitskyi <pashnitskyiRoman@gmail.com>
2021-03-15 20:18:49 +0200

    Updates

---
+++ precache-manifest.3bfc122bdc3b6c5df73f28cf3d4de0dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b78a1fcc946788ce62e29a2fdd72443",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ad537cf1c772be356ba3",
+    "url": "./static/css/main.c2c3cd60.chunk.css"
+  },
+  {
+    "revision": "1fdf9bd4bbbd4352682e",
+    "url": "./static/js/2.527e63ce.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.527e63ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad537cf1c772be356ba3",
+    "url": "./static/js/main.06bc49b0.chunk.js"
+  },
+  {
+    "revision": "47321982d1ad529b80fc",
+    "url": "./static/js/runtime-main.eabc875b.js"
+  }
+]);
